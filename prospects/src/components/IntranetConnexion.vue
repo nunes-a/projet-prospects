dd82c0dc3a009816ef8519d5662b2d0e26494c1a
nunes-a <amsjnunes@live.com.pt>
2019-04-01 15:41:41 +0200

    Création de la base intranet et son router

---
+++ prospects/src/components/IntranetConnexion.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="int">
+    <h1>bonjour!!!</h1>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IntranetConnexion'
+
+}
+</script>
+
+<style scoped>
+h1 {
+  margin: 40px 0 0;
+  color: #42b983;
+}
+
+</style>
---
+++ prospects/src/views/Intranet.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="intranet">
+  	<IntranetConnexion/>
+    <p>connexion</p>
+  </div>
+</template>
+
+<script>
+import IntranetConnexion from '@/components/IntranetConnexion.vue'
+
+export default {
+  name: 'intranet',
+  components: {
+    IntranetConnexion
+  }
+}
+</script>
