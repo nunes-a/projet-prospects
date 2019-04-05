d047f02b15f9ac07dad133b99bfc25f9743efd84
nunes-a <amsjnunes@live.com.pt>
2019-04-05 11:42:31 +0200

    prospescts (list data), icons, axios (pas terminer)

--- src/main.js
+++ src/main.js
@@ -4,8 +4,20 @@ import router from './router'
 import store from './store'
 import './registerServiceWorker'
 
+//elements bootstrap
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+
+//icons fortawesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faEye, faTrashAlt, faPhone } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+library.add(faEye, faTrashAlt, faPhone)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+//
+
+
 
 Vue.config.productionTip = false
 
--- src/views/MesProspects.vue
+++ src/views/MesProspects.vue
@@ -6,25 +6,25 @@
 	<table class="table table-striped">
 	  <thead>
 	    <tr>
-	      <th scope="col">id</th>
 	      <th scope="col">Nom</th>
 	      <th scope="col">Prénom</th>
 	      <th scope="col"></th>
 	      <th scope="col"></th>
-
+	      <th scope="col"></th>
 	    </tr>
 	  </thead>
 	  <tbody>
 	    <tr v-for="prospect in prospects">
-	      <td>{{prospect.id}}</td>
 	      <td>{{prospect.nom}}</td>
 	      <td>{{prospect.prenom}}</td>
-	      <td><button class="btn btn-danger btn-sm" v-on:click="removeProspect(id)">X</button></td>
-	      <td>O</td>
+	      <td><font-awesome-icon  v-on:click="removeProspect(prospect)" icon="trash-alt"/></td>
+	      <td><font-awesome-icon icon="eye"/></td>
+	      <td><font-awesome-icon icon="phone"/></td>
 	    </tr>
 	  </tbody>
 	</table>
 
+	<div>{{ info }}</div>
  
   </div>
 </template>
@@ -43,27 +43,30 @@ export default {
 		return {
 			prospects: [
 		        { 
-		        	id: 1, 
-		            nom: "Nunes",
-		            prenom: "Alexandre"
+		        	id: 1, nom: "Nunes", prenom: "Alexandre"
 		        },
 		        { 
-		            id: 2, 
-		            nom: "José",
-		            prenom: "Miguel"
+		            id: 2, nom: "José", prenom: "Miguel"
 		        }
-	    	]
+	    	],
+	    	info: null
 		}
 	},
 
 	methods: {
-		removeProspect: function(id) {
-        	var index = this.prospects.indexOf(id);
+		removeProspect: function(prospect) {
+        	var index = this.prospects.indexOf(prospect);
             if (index > -1) {
                 this.prospects.splice(index, 1);
             }
         }
-	}
+	},
+
+	mounted () {
+	    axios
+	      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+	      .then(response => (this.info = response))
+	  }
 
 
 }
